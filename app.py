c7cc86d1509f4af2f942373fd0ba7f177cd0411f
Hyunsoo6257 <162527916+Hyunsoo6257@users.noreply.github.com>
2024-05-04 05:24:01 +0000

    Added dynamic data and API

--- app.py
+++ app.py
@@ -1,11 +1,39 @@
-from flask import Flask
+from flask import Flask, render_template, jsonify
 
 app = Flask(__name__)
+
+EXP = [{
+    'id': 1,
+    'title': 'Waiter',
+    'location': 'Outback Steakhouse',
+    'salary': '$30,000'
+}, {
+    'id': 2,
+    'title': 'Frontend Developer',
+    'location': 'Feield-S',
+    'salary': '$50,000'
+}, {
+    'id': 3,
+    'title': 'Sales associate',
+    'location': 'hay',
+    'salary': '$12,000'
+}, {
+    'id': 4,
+    'title': 'Home protector',
+    'location': 'Outback Steakhouse',
+    'salary': '$30,000'
+}]
 
 
 @app.route('/')
 def hello_world():
-  return "Hello, world"
+  return render_template('home.html', exp=EXP)
+
+
+# create json file
+@app.route("/exps")
+def list_exps():
+  return jsonify(EXP)
 
 
 print("Hello, world")
